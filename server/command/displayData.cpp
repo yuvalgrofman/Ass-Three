248d7adcea7bb897e5a3e83a6195aa53d01bb7fc
yuval <yuvalgrofman@gmail.com>
2022-08-21 22:21:30 +0300

    Added displayData command

---
+++ server/command/displayData.cpp
@@ -0,0 +1,19 @@
+#include "displayData.h"
+
+DisplayData::DisplayData(DefaultIO *dio, int userId) : Command(dio, userId) {
+    description.assign("display results");
+}
+
+void DisplayData::execute() {
+    ifstream istream("../server/data/user_" + to_string(userId)
+    + "_train_prediction.csv");
+
+    string word;
+    int lineNumber = 1;
+
+    while(getline(istream, word)) {
+        dio->write(to_string(lineNumber) + " " + word + "\n");
+    }
+
+    dio->write("Done.");
+}
---
+++ server/command/displayData.h
@@ -0,0 +1,13 @@
+#ifndef ASS_THREE_DISPLAYDATA_H
+#define ASS_THREE_DISPLAYDATA_H
+
+#include "command.h"
+
+class DisplayData : Command {
+
+    public:
+        DisplayData(DefaultIO* dio, int userId);
+        virtual void execute() override;
+};
+
+#endif //ASS_THREE_DISPLAYDATA_H
