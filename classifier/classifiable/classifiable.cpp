b35f9c3e18d538fdf1bfce18ebdd8bb8826402e7
OldRimStalker <jonathanmoiseyev@outlook.com>
2022-08-22 18:42:59 +0300

    made the code for the classifiable class

--- classifier/classifiable/classifiable.cpp
+++ classifier/classifiable/classifiable.cpp
@@ -1,5 +1,18 @@
-//
-// Created by jonathan on 8/22/22.
-//
-
 #include "classifiable.h"
+
+Classifiable::Classifiable(const DataPoint &dataPoint, string type) : type(type), dataPoint(dataPoint) {}
+
+Classifiable::Classifiable(Classifiable const &classifiable): type(classifiable.type),
+            dataPoint(classifiable.dataPoint) {}
+
+string Classifiable::getType() const {
+    return type;
+}
+
+const DataPoint& Classifiable::getData() const {
+    return dataPoint;
+}
+
+Classifiable::~Classifiable() {
+    delete &dataPoint;
+}
--- classifier/classifiable/classifiable.h
+++ classifier/classifiable/classifiable.h
@@ -1,8 +1,27 @@
 #ifndef ASS_THREE_CLASSIFIABLE_H
 #define ASS_THREE_CLASSIFIABLE_H
 
-class Classifiable {
+#include <string>
+#include "dataPoint.h"
+using namespace std;
 
+class Classifiable {
+private:
+    const DataPoint& dataPoint;
+    const string type;
+
+public:
+    Classifiable(Classifiable const &classifiable);
+
+    Classifiable(const DataPoint &dataPoint, string type);
+
+    string getType() const;
+
+    const DataPoint& getData() const;
+
+    Classifiable() = default;
+
+    ~Classifiable();
 };
 
 
