04bdb4397ff38675543fa9f98ab8276f8c9aad5c
yuval <yuvalgrofman@gmail.com>
2022-08-22 17:26:44 +0300

    implemented run function

--- server/server.cpp
+++ server/server.cpp
@@ -1,8 +1,36 @@
+#include <algorithm>
 #include "server.h"
 
+map<int, Command*>* getCommands(DefaultIO* dio, int userId) {
+    map<int, Command*>* map = new ::map<int, Command*>();
+
+    map->insert(std::make_pair(1, new RecvData(dio, userId)));
+    map->insert(std::make_pair(2, new KnnSettings(dio, userId)));
+    map->insert(std::make_pair(3, new ClassifyData(dio, userId)));
+    map->insert(std::make_pair(4, new DisplayData(dio, userId)));
+    map->insert(std::make_pair(5, new UploadData(dio, userId)));
+    map->insert(std::make_pair(6, new DisplayConfusionMatrix(dio, userId)));
+    map->insert(std::make_pair(7, new Close(dio, userId)));
+}
 
 Server::Server(DefaultIO *dio, int userId) : dio(dio), userId(userId) {}
 
 void Server::run() {
+    map<int, Command*>* map = getCommands(dio, userId);
+    int op;
 
+    while (op != 7) {
+        bool validInput = false;
+
+        do {
+            string in = dio->read();
+
+            if (!in.empty() && std::all_of(in.begin(), in.end(), ::isdigit)) {
+                op = stoi(in);
+                validInput = 0 < op && op < 11;
+            }
+        } while (!validInput);
+
+        map->at(op)->execute();
+    }
 }
