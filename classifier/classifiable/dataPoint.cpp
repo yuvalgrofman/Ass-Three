0c333de5655c2cd0f2a44e9c1a37f54a159818ed
OldRimStalker <jonathanmoiseyev@outlook.com>
2022-08-23 13:55:28 +0300

    made another constructor for the datapoint class

--- classifier/classifiable/dataPoint.cpp
+++ classifier/classifiable/dataPoint.cpp
@@ -2,6 +2,8 @@
 
 DataPoint::DataPoint(const double data[], int size) : data(data), size(size) {}
 
+DataPoint::DataPoint(const vector<double> &data) : data(data.data()), size(data.size()) {}
+
 double DataPoint::getVariable(int index) const {
     return data[index];
 }
@@ -10,3 +12,6 @@ int DataPoint::getNumberOfVariables() const {
     return size;
 }
 
+DataPoint::~DataPoint() {
+    delete[] data;
+}
--- classifier/classifiable/dataPoint.h
+++ classifier/classifiable/dataPoint.h
@@ -13,6 +13,8 @@ class DataPoint {
         DataPoint(const double data[], int size);
         double getVariable(int index) const;
         int getNumberOfVariables() const;
+        DataPoint(const vector<double> &data);
+        ~DataPoint();
 };
 
 
