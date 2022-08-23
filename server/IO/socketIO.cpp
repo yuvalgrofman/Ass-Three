7d636ea409916803fb8486c6277b92c867afe40c
OldRimStalker <jonathanmoiseyev@outlook.com>
2022-08-23 17:36:26 +0300

    made the code for socketIO

--- server/IO/socketIO.cpp
+++ server/IO/socketIO.cpp
@@ -1,2 +1,59 @@
 #include "socketIO.h"
 
+SocketIO::SocketIO(int port) {
+    this->port = port;
+
+    this->sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sock < 0) {
+        perror("error creating socket");
+    }
+
+    struct sockaddr_in sin;
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(this->port);
+
+    if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
+        perror("error binding socket");
+    }
+
+    if (listen(sock, 5) < 0) {
+        perror("error listening to a socket");
+    }
+}
+
+void SocketIO::connect() {
+    struct sockaddr_in client_sin;
+    unsigned int addr_len = sizeof(client_sin);
+
+    this->client_sock = accept(sock,  (struct sockaddr *) &client_sin,  &addr_len);
+
+    if (client_sock < 0) {
+        perror("error accepting client");
+    }
+}
+
+string SocketIO::read() const {
+    char buffer[BUFFER_SIZE];
+    int read_bytes = recv(client_sock, buffer, BUFFER_SIZE, 0);
+    if (read_bytes == 0) {
+        // connection closed
+        return "";
+    } else if (read_bytes < 0) {
+        perror("error receiving flower point");
+    }
+    return string(buffer);
+}
+
+void SocketIO::write(string str) const {
+    int sent_bytes = send(client_sock, str.c_str(), str.length(), 0);
+    if (sent_bytes < 0) {
+        perror("error sending flower point");
+    }
+}
+
+void SocketIO::close() const {
+    ::close(sock);
+}
--- server/IO/socketIO.h
+++ server/IO/socketIO.h
@@ -2,12 +2,31 @@
 #define ASS_THREE_SOCKETIO_H
 
 #include "defaultIO.h"
+#include <iostream>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 
 class SocketIO : DefaultIO {
+    private:
+
+    int port;
+        int sock;
+        int client_sock;
+
+        void connect();
+
     public:
+        static const int BUFFER_SIZE = 4096;
+
+        SocketIO(int port = 5555);
+
         virtual string read() const override;
         virtual void write(string str) const override;
-        virtual void close() const override;
+        void close() const;
 };
 
 
