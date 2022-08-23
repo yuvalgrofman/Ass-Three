7508e165f7984d454636bf9bdb49d455fd2b2531
OldRimStalker <jonathanmoiseyev@outlook.com>
2022-08-23 13:55:00 +0300

    made the code for the data reader class

--- classifier/classifiable/dataReader.cpp
+++ classifier/classifiable/dataReader.cpp
@@ -1,5 +1,68 @@
-//
-// Created by jonathan on 8/22/22.
-//
-
 #include "dataReader.h"
+#include <sstream>
+#include <iostream>
+
+DataPoint* detectDataPoint(const string &line) {
+    string word;
+
+    if (line == "")
+        return nullptr;
+
+    stringstream streamLine(line);
+
+    vector<double> data;
+
+    while(getline(streamLine, word, ',')) {
+        double value = 0;
+        try {
+            value = stod(word);
+        } catch (...) {
+            break;
+        }
+        data.push_back(stod(word));
+    }
+
+    double *arr;
+    int size = data.size();
+    arr = new double[size];
+    for (int i = 0; i < size; i++) {
+        arr[i] = data.at(i);
+    }
+
+    return new DataPoint(arr, size);
+}
+
+Classifiable* detectClassifiable(const string &line) {
+    if (line == "")
+        return nullptr;
+
+    string type = line.substr(line.find_last_of(',') + 1);
+    string data = line.substr(0, line.find_last_of(','));
+
+    if (data.empty() || type.empty()){
+        return nullptr;
+    }
+
+    DataPoint *dataPoint = detectDataPoint(data);
+    if (dataPoint == nullptr) {
+        return nullptr;
+    }
+
+    return new Classifiable(*dataPoint, type);
+}
+
+DataReader::DataReader(const string &filename) : file(filename) {
+        fin.open(file, ios::in);
+}
+
+Classifiable* DataReader::readClassifiable() {
+    string line;
+    getline(fin, line);
+    return detectClassifiable(line);
+}
+
+DataPoint* DataReader::readDataPoint() {
+    string line;
+    getline(fin, line);
+    return detectDataPoint(line);
+}
--- classifier/classifiable/dataReader.h
+++ classifier/classifiable/dataReader.h
@@ -1,9 +1,21 @@
 #ifndef ASS_THREE_DATAREADER_H
 #define ASS_THREE_DATAREADER_H
 
+#include "classifiable.h"
+#include <fstream>
+
+DataPoint* detectDataPoint(const string &line);
+Classifiable* detectClassifiable(const string &line);
 
 class DataReader {
+    private:
+        string file;
+        fstream fin;
 
+    public:
+        DataReader(const string &filename);
+        Classifiable* readClassifiable();
+        DataPoint* readDataPoint();
 };
 
 
