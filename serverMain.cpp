51297b492569c0c0c608b69559b8859e45be2949
yuval <yuvalgrofman@gmail.com>
2022-08-25 14:49:55 +0300

    changed main to serverMain.cpp

---
+++ serverMain.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "server/server.h"
+#include "server/IO/standardIO.h"
+#include "server/serverSocket.h"
+
+int main() {
+
+    ServerSocket* ss = new ServerSocket();
+    DefaultIO* dio = ss->accept();
+    Server* s = new Server(dio, 1);
+    s->run();
+    s->close();
+}
