8d7ae1b543f0db027dbd61bb1c93ff6815c42db7
OldRimStalker <jonathanmoiseyev@outlook.com>
2022-08-25 17:50:27 +0300

    added documentation to the server-thread function.

--- server/serverThread.h
+++ server/serverThread.h
@@ -1,6 +1,13 @@
 #ifndef ASS_THREE_SERVERTHREAD_H
 #define ASS_THREE_SERVERTHREAD_H
 
-void *serverThread(void *vargp);
+
+/**
+ * the method each thread will run.
+ * it will run the server of a specific user.
+ * @param arg - the argument sent to the thread,
+ * it should be a pointer to a Server object.
+ */
+void *serverThread(void *arg);
 
 #endif //ASS_THREE_SERVERTHREAD_H
