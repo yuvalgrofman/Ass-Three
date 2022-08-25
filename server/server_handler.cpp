49e3c0c76d396c2bc8540513c06eb84adcdaca15
Jonathan Moiseyev <jonathanmoiseyev@outlook.com>
2022-08-25 15:22:02 +0300

    started working on the code of the server handler

--- server/server_handler.cpp
+++ server/server_handler.cpp
@@ -1,30 +1,37 @@
 #include "server_handler.h"
 
-//Server_handler::Server_handler(int port) {
-//    server = new Server(port);
-//}
-//
-//void Server_handler::connect() {
-//    server->connect();
-//}
-//
-//void Server_handler::run() {
-//    Distance* d = new EuclideanDistance();
-//
-//    while (!server->isCommOver()) {
-//        FlowerPoint& flowerPoint = server->receiveFlowerPoint();
-//        if (!server->isCommOver())
-//            server->sendClassification(flowerPoint, 3, d);
-//        delete &flowerPoint;
-//    }
-//
-//    delete d;
-//}
-//
-//void Server_handler::close() {
-//    server->closeServer();
-//}
-//
-//Server_handler::~Server_handler() {
-//    delete server;
-//}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "serverThread.h"
+#include <pthread.h>
+
+Server_handler::Server_handler(SocketIO *io) {
+    this->io = io;
+}
+
+void Server_handler::run() {
+    while(true) {
+        try {
+            string s = io->read();
+            int id = getNewId();
+            int port = getNewPort();
+            io->write(to_string(port));
+            io->write(to_string(id));
+
+            int input[2] = {port, id};
+
+            pthread_t thread_id;
+
+            pthread_create(&thread_id, NULL, serverThread, (void *)&port);
+            pthread_join(thread_id, NULL);
+        } catch (...) {
+            // do nothing.
+        }
+    }
+}
+
+void Server_handler::close() {
+    io->close();
+}
