1b0bcd1fd766a22aae29ccecdb5bdd4a7f977c52
OldRimStalker <jonathanmoiseyev@outlook.com>
2022-08-25 16:10:21 +0300

    changing the function that each thread executes

--- server/serverThread.cpp
+++ server/serverThread.cpp
@@ -3,14 +3,10 @@
 #include "IO/socketIO.h"
 
 void* serverThread(void *vargp) {
-    int *input = (int *)vargp;
-    int port = input[0];
-    int id = input[1];
-    SocketIO *io = new SocketIO(port);
-    Server server(io, id);
+    Server *server = (Server *)vargp;
 
-    server.run();
-    server.close();
+    server->run();
+    server->close();
 
-    delete io;
+    delete server;
 }
