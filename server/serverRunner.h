6241b1f25d8b9297e1f68eeb5b388b93139e26be
OldRimStalker <jonathanmoiseyev@outlook.com>
2022-08-25 17:50:05 +0300

    added documentation to the server-runner class.

--- server/serverRunner.h
+++ server/serverRunner.h
@@ -4,13 +4,27 @@
 #include "serverSocket.h"
 #include "vector"
 
+/**
+ * @brief The ServerRunner class
+ *
+ * This class is responsible for creating new servers in different threads
+ * with the appropriate instance of DefaultIO and userId.
+ */
 class ServerRunner {
     private:
         int highestId;
         ServerSocket &serverSocket;
 
         public:
+            /**
+             * Constructor.
+             * @param serverSocket - the server socket to use
+             */
             ServerRunner(ServerSocket &serverSocket);
+
+            /**
+             * Runs the server runner.
+             */
             void run();
 };
 
