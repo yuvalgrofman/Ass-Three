3b6f2f63b1f58ad6b9e140d80facc3f44be5f42f
yuval <yuvalgrofman@gmail.com>
2022-08-25 14:49:10 +0300

    created main for server and client and changed CMakeLists.txt accordingly

---
+++ clientMain.cpp
@@ -0,0 +1,10 @@
+#include "client/clientSocket.h"
+#include "client/client.h"
+
+int main() {
+
+    ClientSocket* dio = new ClientSocket();
+    Client* c = new Client(dio);
+    c->run();
+    c->close();
+}
