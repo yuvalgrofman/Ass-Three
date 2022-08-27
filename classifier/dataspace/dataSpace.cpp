d286ea680d44479ff1bbbd0b2cc6741ffb052fb3
OldRimStalker <jonathanmoiseyev@outlook.com>
2022-08-27 19:05:29 +0300

    implemented a more efficient method of classification

--- classifier/dataspace/dataSpace.cpp
+++ classifier/dataspace/dataSpace.cpp
@@ -32,39 +32,24 @@ string DataSpace::predict(int k, const DataPoint& dataPoint, Distance& distance)
         classifications.push_back(arr[i]->getType());
     }
 
+    map<string, int> classificationCount;
     int maxCount = 0;
     string maxType = "";
     for (int i = 0; i < k; i++) {
-        int count = 0;
-        for (int j = 0; j < k; j++) {
-            if (classifications.at(j) == classifications.at(i)) {
-                count++;
-            }
+        string type = arr[i]->getType();
+
+        if (classificationCount.find(type) == classificationCount.end()) {
+            classificationCount.insert(pair<string, int>(type, 1));
+        } else {
+            classificationCount.at(type)++;
         }
+
+        int count = classificationCount.at(type);
         if (count > maxCount) {
             maxCount = count;
-            maxType = classifications.at(i);
+            maxType = type;
         }
     }
-
-//    map<string, int> classificationCount;
-//    int maxCount = 0;
-//    string maxType = "";
-//    for (int i = 0; i < k; i++) {
-//        string type = arr[i]->getType();
-//
-//        if (classificationCount.find(type) == classificationCount.end()) {
-//            classificationCount.insert(pair<string, int>(type, 1));
-//        } else {
-//            classificationCount.emplace(type, classificationCount.at(type) + 1);
-//        }
-//
-//        int count = classificationCount.at(type);
-//        if (count > maxCount) {
-//            maxCount = count;
-//            maxType = type;
-//        }
-//    }
 
     return maxType;
 }
