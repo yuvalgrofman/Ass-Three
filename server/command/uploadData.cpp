99127769f8563aafd2a03268cb04dc4a3ffb1e11
yuval <yuvalgrofman@gmail.com>
2022-08-28 00:17:00 +0300

    changed order of communication on files and changed client appropriately

--- server/command/uploadData.cpp
+++ server/command/uploadData.cpp
@@ -3,33 +3,26 @@
 UploadData::UploadData(DefaultIO *dio, int userId) : Command(dio, userId, "download results") {}
 
 void UploadData::execute() {
+    if (!isDataClassified()) {
+        dio->write("The data hasn't been classified.\nTo classify the data press 3.\n");
+        return;
+    } else {
+        dio->write("Sending data.\n");
+    }
+
     ifstream istream;
     string buffer;
 
-    string trainFileContent;
     string testFileContent;
 
-    istream.open("../server/data/user_" + to_string(userId)+ "_train_prediction.csv");
+    istream.open("../server/data/user_" + to_string(userId)+ "_test_prediction.csv");
 
     if (!istream.is_open()) {
         //TODO: print error
     }
 
     while (getline(istream, buffer)) {
-        trainFileContent += buffer;
-    }
-
-    dio->write(trainFileContent);
-
-    istream.open("../server/data/user_" + to_string(userId)
-                                    + "_train_prediction.csv");
-
-    if (!istream.is_open()) {
-        //TODO: print error
-    }
-
-    while (getline(istream, buffer)) {
-        testFileContent += buffer;
+        testFileContent += buffer + "\n";
     }
 
     dio->write(testFileContent);
