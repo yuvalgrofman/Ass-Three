94f37e46ed5e8ce2c1e554300c78bf15034348c3
OldRimStalker <jonathanmoiseyev@outlook.com>
2022-09-03 13:40:25 +0300

    changed the confusion matrix to not need the files to be predicted before it is run. and changed it, so it will delete the train prediction file, after it is done

--- server/command/displayConfusionMatrix.cpp
+++ server/command/displayConfusionMatrix.cpp
@@ -34,99 +34,105 @@ DisplayConfusionMatrix::DisplayConfusionMatrix(DefaultIO* dio, int userId) : Com
     "display algorithm confusion matrix") {}
 
 void DisplayConfusionMatrix::execute() {
-    if (!isDataClassified()) {
-        dio->write("The data hasn't been classified.\nTo classify the data press 3.\n");
-        return;
+    string train = "../server/data/user_" + to_string(userId) + "_train.csv";
+    bool trainIsEmpty = false;
+    ifstream trainContentStream(train);
+    trainIsEmpty = trainContentStream.peek() == std::ifstream::traits_type::eof();
+
+    if (trainIsEmpty) {
+        string s = "train file is empty.\nIn order to upload it choose option 1 from the option screen.\n";
+        dio->write(s);
     } else {
         dio->write("Sending data.\n");
-    }
 
-    string train = "../server/data/user_" + to_string(userId) + "_train.csv";
-    Classifier *c = new Classifier(getK(), train, train);
+        Classifier *c = new Classifier(getK(), train, train);
 
-    Distance *d = getDistance();
-    c->predictFileByDist("../server/data/user_" + to_string(userId)
-                         + "_train_prediction.csv", *d);
-    delete d;
-    delete c;
+        Distance *d = getDistance();
+        c->predictFileByDist("../server/data/user_" + to_string(userId)
+                             + "_train_prediction.csv", *d);
+        delete d;
+        delete c;
 
-    map<string, int> &types = getClassificationOptions();
+        map<string, int> &types = getClassificationOptions();
 
-    int numTypes = types.size();
-    int results[numTypes][numTypes];
-    for (int i = 0; i < numTypes; i++) {
-        for (int j = 0; j < numTypes; j++) {
-            results[i][j] = 0;
+        int numTypes = types.size();
+        int results[numTypes][numTypes];
+        for (int i = 0; i < numTypes; i++) {
+            for (int j = 0; j < numTypes; j++) {
+                results[i][j] = 0;
+            }
         }
-    }
 
-    string prediction = "../server/data/user_" + to_string(userId) + "_train_prediction.csv";
+        string prediction = "../server/data/user_" + to_string(userId) + "_train_prediction.csv";
 
-    ifstream predictionStream(prediction);
-    ifstream trainStream(train);
+        ifstream predictionStream(prediction);
+        ifstream trainStream(train);
 
-    string predictionLine;
-    string trainLine;
-    getline(predictionStream, predictionLine);
-    getline(trainStream, trainLine);
-
-    while (!predictionStream.eof() && !trainStream.eof()) {
-        trainLine = trainLine.substr(trainLine.find_last_of(',') + 1);
-
-        int predictionIndex = types.at(predictionLine);
-
-        int trainIndex = types.at(trainLine);
-
-        results[trainIndex][predictionIndex]++;
-
-
+        string predictionLine;
+        string trainLine;
         getline(predictionStream, predictionLine);
         getline(trainStream, trainLine);
-    }
 
-    predictionStream.close();
-    trainStream.close();
+        while (!predictionStream.eof() && !trainStream.eof()) {
+            trainLine = trainLine.substr(trainLine.find_last_of(',') + 1);
 
-    for (int row = 0; row < numTypes; row++) {
-        int numGussesInRow = 0;
-        for (int col = 0; col < numTypes; col++) {
-            numGussesInRow += results[row][col];
+            int predictionIndex = types.at(predictionLine);
+
+            int trainIndex = types.at(trainLine);
+
+            results[trainIndex][predictionIndex]++;
+
+
+            getline(predictionStream, predictionLine);
+            getline(trainStream, trainLine);
         }
 
-        for (int col = 0; col < numTypes; col++) {
-            results[row][col] = round((double(results[row][col]) / double(numGussesInRow)) * 100);
+        predictionStream.close();
+        trainStream.close();
+
+        for (int row = 0; row < numTypes; row++) {
+            int numGussesInRow = 0;
+            for (int col = 0; col < numTypes; col++) {
+                numGussesInRow += results[row][col];
+            }
+
+            for (int col = 0; col < numTypes; col++) {
+                results[row][col] = round((double(results[row][col]) / double(numGussesInRow)) * 100);
+            }
         }
-    }
 
-    dio->write("Confusion Matrix:");
+        dio->write("Confusion Matrix:");
 
-    int maxLengthClassificationString = 0;
-    map<string, int>::iterator itr;
-    for (itr = types.begin(); itr != types.end(); itr++) {
-        int length = itr->first.length();
-        if (maxLengthClassificationString < length) {
-            maxLengthClassificationString = length;
+        int maxLengthClassificationString = 0;
+        map<string, int>::iterator itr;
+        for (itr = types.begin(); itr != types.end(); itr++) {
+            int length = itr->first.length();
+            if (maxLengthClassificationString < length) {
+                maxLengthClassificationString = length;
+            }
         }
-    }
 
-    for (itr = types.begin(); itr != types.end(); itr++) {
-        int row = itr->second;
+        for (itr = types.begin(); itr != types.end(); itr++) {
+            int row = itr->second;
 
 
-        string rowString = itr->first;
-        int length = rowString.length();
-        rowString = rowString.append(maxLengthClassificationString - length, ' ');
+            string rowString = itr->first;
+            int length = rowString.length();
+            rowString = rowString.append(maxLengthClassificationString - length, ' ');
 
-        for (int col = 0; col < numTypes; col++) {
-            rowString += "\t" + to_string(results[row][col]);
+            for (int col = 0; col < numTypes; col++) {
+                rowString += "\t" + to_string(results[row][col]);
+            }
+            dio->write(rowString);
         }
-        dio->write(rowString);
-    }
 
-    Distance *dist = getDistance();
-    dio->write("The current KNN parameters are: K = " + to_string(getK())
-               + ", distance metric = " + dist->getName());
-    delete dist;
-    
-    delete &types;
+        remove((prediction).c_str());
+
+        Distance *dist = getDistance();
+        dio->write("The current KNN parameters are: K = " + to_string(getK())
+                   + ", distance metric = " + dist->getName());
+        delete dist;
+
+        delete &types;
+    }
 }
